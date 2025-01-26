8d9707aa2cd768dafda3c6859322988cf8cdb3c6
Angel75Git <aduboncenteno@usf.edu>
2025-01-26 08:37:14 -0500

    Task 3 - Financial Transactions.

--- cc_1.js
+++ cc_1.js
@@ -13,3 +13,18 @@ var isAvialable = false
 console.log(productName, typeof(productName))
 console.log(productPrice, typeof(productPrice))
 console.log(isAvialable, typeof(isAvialable))
+
+let accountBalance = 1223
+
+console.log("accountBalance update: ", "$",accountBalance + 1425 ) 
+accountBalance = accountBalance + 1425
+
+console.log("accountBalance update: ", "$",accountBalance - 382 )
+accountBalance = accountBalance - 382  
+              
+console.log("accountBalance update: ", "$",accountBalance * 15 )
+accountBalance = accountBalance * 15    
+                  
+console.log("accountBalance update: ", "$",accountBalance / 10 ) 
+accountBalance = accountBalance / 10  
+                                  
