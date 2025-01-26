4a233ecf03e48c04d57d711e61543b326e7280e5
Angel75Git <aduboncenteno@usf.edu>
2025-01-26 08:11:33 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,10 @@ console.log(employeeName, typeof(employeeName))
 console.log(employeeID, typeof(employeeID))
 console.log(isActive, typeof(isActive))
 
+let productName = "Shampoo"
+const productPrice = 9.49
+var isAvialable = false
+
+console.log(productName, typeof(productName))
+console.log(productPrice, typeof(productPrice))
+console.log(isAvialable, typeof(isAvialable))
