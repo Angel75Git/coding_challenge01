53e4d0dac88260b3e9aea2c0861364c38ef70c2e
Angel75Git <aduboncenteno@usf.edu>
2025-01-26 08:00:52 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+let employeeName = "John"
+const employeeID = 48752
+var isActive = true 
+
+console.log(employeeName, typeof(employeeName))
+console.log(employeeID, typeof(employeeID))
+console.log(isActive, typeof(isActive))
+
